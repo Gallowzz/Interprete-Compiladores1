6293c41a4a1237074c45286fd7d011cceee8e470
Gallowzz <134352607+Gallowzz@users.noreply.github.com>
2025-11-06 18:44:25 -0600

    Creacion
    En el inicio, Claudio creo el main y el gitignore. El build tambien, pero ese no se subio.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect Argument Amount\n";
+        return 1;
+    }
+
+    std::ifstream in(argv[1]);
+    if (!in.is_open()) {
+        std::cerr << "Cannot open file\n";
+        return 1;
+    }
+
+    return 0;
+}
